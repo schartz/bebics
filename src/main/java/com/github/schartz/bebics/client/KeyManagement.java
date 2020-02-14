0ea21254da467902c6c8807901d53d3a6e02c958
Schartz Rehan <rehan.qt@gmail.com>
2020-02-14 17:43:18 +0530

    Bump the version of XMLbeans to 3.1.0. Apply a quick fix for the issue of bank certificate use being misinterpreted

--- src/main/java/com/github/schartz/bebics/client/KeyManagement.java
+++ src/main/java/com/github/schartz/bebics/client/KeyManagement.java
@@ -139,8 +139,10 @@ public class KeyManagement {
     keystoreManager = new KeyStoreManager();
     path = session.getConfiguration().getKeystoreDirectory(session.getUser());
     keystoreManager.load("" , session.getUser().getPasswordCallback().getPassword());
+    System.out.println("Check if bank uses cerificates");
     System.out.println(this.session.getUser().getPartner().getBank().useCertificate());
-    if (this.session.getUser().getPartner().getBank().useCertificate()) {
+    /*if (this.session.getUser().getPartner().getBank().useCertificate()) {
+      System.out.println("Bank uses certificate");
       e002PubKey = keystoreManager.getPublicKey(new ByteArrayInputStream(orderData.getBankE002Certificate()));
       x002PubKey = keystoreManager.getPublicKey(new ByteArrayInputStream(orderData.getBankX002Certificate()));
       this.session.getUser().getPartner().getBank().setBankKeys(e002PubKey, x002PubKey);
@@ -149,12 +151,25 @@ public class KeyManagement {
       keystoreManager.setCertificateEntry(this.session.getBankID() + "-X002", new ByteArrayInputStream(orderData.getBankX002Certificate()));
       keystoreManager.save(new FileOutputStream(path + File.separator + this.session.getBankID() + ".p12"));
     } else {
+      System.out.println("Bank does not use certificate");
       e002PubKey = keystoreManager.getPublicKey(new BigInteger(orderData.getBankE002PublicKeyExponent()), new BigInteger(orderData.getBankE002PublicKeyModulus()));
       x002PubKey = keystoreManager.getPublicKey(new BigInteger(orderData.getBankX002PublicKeyExponent()), new BigInteger(orderData.getBankX002PublicKeyModulus()));
       this.session.getUser().getPartner().getBank().setBankKeys(e002PubKey, x002PubKey);
       this.session.getUser().getPartner().getBank().setDigests(KeyUtil.getKeyDigest(e002PubKey), KeyUtil.getKeyDigest(x002PubKey));
       keystoreManager.save(new FileOutputStream(path + File.separator + this.session.getBankID() + ".p12"));
-    }
+    }*/
+
+    /**
+     *
+     * This is an ugly hack!
+     * come back and fix it
+     * **/
+    System.out.println("Proceeding with a quick fix implementation!");
+    e002PubKey = keystoreManager.getPublicKey(new BigInteger(orderData.getBankE002PublicKeyExponent()), new BigInteger(orderData.getBankE002PublicKeyModulus()));
+    x002PubKey = keystoreManager.getPublicKey(new BigInteger(orderData.getBankX002PublicKeyExponent()), new BigInteger(orderData.getBankX002PublicKeyModulus()));
+    this.session.getUser().getPartner().getBank().setBankKeys(e002PubKey, x002PubKey);
+    this.session.getUser().getPartner().getBank().setDigests(KeyUtil.getKeyDigest(e002PubKey), KeyUtil.getKeyDigest(x002PubKey));
+    keystoreManager.save(new FileOutputStream(path + File.separator + this.session.getBankID() + ".p12"));
   }
 
   /**
